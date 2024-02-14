a8d83c26e20ddd3b07fc7c7fbbd68d021fffc904
vishnu ganagi <01fe22bca225@kletech.ac.in>
2024-02-14 15:15:27 +0530

    adding two file for extended calculator branch

---
+++ extended.java
@@ -0,0 +1,20 @@
+public class extended {
+    public int areaoftri(){
+        int height=8;
+        int base=4;
+        int area_of_tri=(1/2)*base*height;
+        return area_of_tri;
+    } 
+    public int araeofsquare(){
+        int side1=7;
+        int side2=3;
+        int area_of_square=side1*side2;
+        return area_of_square;
+    }
+    public int modulus(){
+        int a=8;
+        int b=2;
+        int mod=a%b;
+        return mod;
+    } 
+}
